e0b00da4ca60f1866f30969cb62558db852dc8e7
Your Name <your-email@example.com>
2025-05-11 07:44:46 +0530

    Add react-bootstrap and fix config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "bootstrap/scss/bootstrap";`
+      }
+    }
   }
 });
